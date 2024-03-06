04ef3b23b2ee33ca9d1a4aff5af707889c649778
Harshvardhansinh Jadeja <harshvardhansinhjadeja49@gmail.com>
2024-03-06 17:46:49 +0530

    Final code with modules

--- Infrastructure/ecr.tf
+++ Infrastructure/ecr.tf
@@ -1,16 +1,16 @@
-module "harshvardhan-repo" {
-  source = "./modules/ecr"
-  name = "harshvardhan-repo"
-  force_delete = true
-}
+# module "harshvardhan-repo" {
+#   source = "./modules/ecr"
+#   name = "harshvardhan-repo"
+#   force_delete = true
+# }
 
 
-resource "null_resource" "run_comand_one" {
-  depends_on = [ module.harshvardhan-repo]
-  provisioner "local-exec" {
-    command = "bash ./ecr.sh  ${var.region} ${var.profile} ${var.account_id} ${var.local-image} ${module.harshvardhan-repo.repository_url}"
-  }
-}
+# resource "null_resource" "run_comand_one" {
+#   depends_on = [ module.harshvardhan-repo]
+#   provisioner "local-exec" {
+#     command = "bash ./ecr.sh  ${var.region} ${var.profile} ${var.account_id} ${var.local-image} ${module.harshvardhan-repo.repository_url}"
+#   }
+# }
 
 # resource "null_resource" "run_comand_one" {
 #   depends_on = [ module.harshvardhan-repo ]
