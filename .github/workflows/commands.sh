447b550f914654ef0536264d413bcafd7622f7ce
Harshvardhansinh Jadeja <harshvardhansinhjadeja49@gmail.com>
2024-03-07 14:44:27 +0530

    added profile

--- .github/workflows/commands.sh
+++ .github/workflows/commands.sh
@@ -4,7 +4,9 @@
 # aws configure --profile ${AWS_PROFILE} set region ${AWS_REGION}
 
 
-aws configure --profile harsh  set aws_access_key_id $AWS_ACCESS_KEY_ID
-aws configure --profile harsh  set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
-aws configure --profile harsh set region us-west-2
+# aws configure --profile harsh  set aws_access_key_id 
+# aws configure --profile harsh  set aws_secret_access_key 
+# aws configure --profile harsh set region us-west-2
+
 aws configure list
+aws sts get-caller-identity
