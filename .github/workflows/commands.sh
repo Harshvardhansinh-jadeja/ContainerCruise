ccafc75abc2a02d5440291ddc4153a9c36e5261a
Harshvardhansinh Jadeja <harshvardhansinhjadeja49@gmail.com>
2024-03-07 14:51:32 +0530

    final push yaml

--- .github/workflows/commands.sh
+++ .github/workflows/commands.sh
@@ -4,7 +4,7 @@
 # aws configure --profile ${AWS_PROFILE} set region ${AWS_REGION}
 
 
-aws configure --profile harsh  set aws_access_key_id $AWS_ACCESS_KEY_ID
-aws configure --profile harsh  set aws_secret_access_key $AWS_SECRET_ACCESS_KEY 
-aws configure --profile harsh set region us-west-2
+aws configure --profile $AWS_PROFILE  set aws_access_key_id $AWS_ACCESS_KEY_ID
+aws configure --profile $AWS_PROFILE  set aws_secret_access_key $AWS_SECRET_ACCESS_KEY 
+aws configure --profile $AWS_PROFILE  set region $AWS_REGION
 aws configure list
