b94b9870b086cae0630e946d295e8fb1a520d54a
Harshvardhansinh Jadeja <harshvardhansinhjadeja49@gmail.com>
2024-03-13 14:44:24 +0530

    condition checking

--- Infrastructure/ecr.tf
+++ Infrastructure/ecr.tf
@@ -3,6 +3,7 @@
 #   name = "harshvardhan-repo"
 #   force_delete = true
 # }
+
 # resource "null_resource" "run_comand_one" {
 #   depends_on = [ module.harshvardhan-repo]
 #   provisioner "local-exec" {
