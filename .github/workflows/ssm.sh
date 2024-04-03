02087cf9d76f62e16582d6bf36d5019d842b53d8
Harshvardhansinh Jadeja <harshvardhansinhjadeja49@gmail.com>
2024-04-03 15:23:13 +0530

    added tfvars in run time

--- .github/workflows/ssm.sh
+++ .github/workflows/ssm.sh
@@ -1,7 +1,5 @@
 # #!/bin/bash
 
-# PARAMS = ["username", "password"]
-
 # SSM_USER=$(aws --region=us-west-2 ssm get-parameter --name "/harshvardhan/sandbox/rds/username" --with-decryption --output text --query Parameter.Value)
 
 # SSM_PASSWORD=$(aws --region=us-west-2 ssm get-parameter --name "/harshvardhan/sandbox/rds/password" --with-decryption --output text --query Parameter.Value)
@@ -15,9 +13,10 @@
 PARAMS=("username" "password")
 REGION="us-west-2"
 PREFIX="/harshvardhan/sandbox/rds/"
+TFVARS_FILE="terraform.tfvars"
 
 for param in "${PARAMS[@]}"; do
     value=$(aws --region="$REGION" ssm get-parameter --name "$PREFIX$param" --with-decryption --output text --query Parameter.Value)
-    
-    echo "TF_VAR_$param=${value}" >> $GITHUB_ENV
+    # echo "TF_VAR_$param=${value}" >> $GITHUB_ENV
+    echo "$param = \"$value\"" >> "$TFVARS_FILE"
 done
