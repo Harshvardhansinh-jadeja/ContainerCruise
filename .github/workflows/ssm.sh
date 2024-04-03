2f4f4f4025f92fbaa99b9db5ddc9aa8bea31ab24
Harshvardhansinh Jadeja <harshvardhansinhjadeja49@gmail.com>
2024-04-03 14:40:36 +0530

    added for loop

--- .github/workflows/ssm.sh
+++ .github/workflows/ssm.sh
@@ -1,9 +1,23 @@
+# #!/bin/bash
+
+# PARAMS = ["username", "password"]
+
+# SSM_USER=$(aws --region=us-west-2 ssm get-parameter --name "/harshvardhan/sandbox/rds/username" --with-decryption --output text --query Parameter.Value)
+
+# SSM_PASSWORD=$(aws --region=us-west-2 ssm get-parameter --name "/harshvardhan/sandbox/rds/password" --with-decryption --output text --query Parameter.Value)
+
+# echo "TF_VAR_username=${SSM_USER}" >> $GITHUB_ENV
+# echo "TF_VAR_password=${SSM_PASSWORD}" >> $GITHUB_ENV
+
+
 #!/bin/bash
 
-SSM_USER=$(aws --region=us-west-2 ssm get-parameter --name "/harshvardhan/sandbox/rds/username" --with-decryption --output text --query Parameter.Value)
+PARAMS=("username" "password")
+REGION="us-west-2"
+PREFIX="/harshvardhan/sandbox/rds/"
 
-SSM_PASSWORD=$(aws --region=us-west-2 ssm get-parameter --name "/harshvardhan/sandbox/rds/password" --with-decryption --output text --query Parameter.Value)
-
-echo "TF_VAR_username=${SSM_USER}" >> $GITHUB_ENV
-echo "TF_VAR_password=${SSM_PASSWORD}" >> $GITHUB_ENV
-
+for param in "${PARAMS[@]}"; do
+    value=$(aws --region="$REGION" ssm get-parameter --name "$PREFIX$param" --with-decryption --output text --query Parameter.Value)
+    
+    echo "TF_VAR_$param=${value}" >> $GITHUB_ENV
+done
--- Infrastructure/main.tf
+++ Infrastructure/main.tf
@@ -1,9 +1,21 @@
+
+terraform {
+  required_providers {
+    aws = {
+      version = "~> 5.35.0"
+    }
+  }
+
+  required_version = "~> 1.7.0"
+}
+
 provider "aws" {
   # profile = "harshvardhan"
   region  = var.region
   # you can use multiple accounts to access this infastructure using this
   # allowed_account_ids = 
 }
+
 
 
 data "aws_iam_role" "task_exec-role" {
