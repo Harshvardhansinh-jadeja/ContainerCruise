315864f22f8c67a6771da7fd45442cce28c741b5
Harshvardhansinh Jadeja <harshvardhansinhjadeja49@gmail.com>
2024-04-03 11:02:33 +0530

    SSM parameter in pipeline testing

--- .github/workflows/ssm.sh
+++ .github/workflows/ssm.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-SSM_USER=aws --region=us-west-2 ssm get-parameter --name "/harshvardhan/sandbox/rds/username" --with-decryption --output text --query Parameter.Value
+SSM_USER= aws --region=us-west-2 ssm get-parameter --name "/harshvardhan/sandbox/rds/username" --with-decryption --output text --query Parameter.Value
 
 echo "TF_VAR_username=${SSM_USER}" >> $GITHUB_ENV
